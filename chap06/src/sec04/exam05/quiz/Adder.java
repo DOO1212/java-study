20a3155193f2f451c071d859fc7f834b91bbaf8d
DOO1212 <ejejgus@naver.com>
2025-05-20 16:14:03 +0900

    tt

---
+++ chap06/src/sec04/exam05/quiz/Adder.java
@@ -0,0 +1,20 @@
+package sec04.exam05.quiz;
+
+public class Adder {
+		
+		void add(int n1, int n2) {
+			System.out.println(n1 + n2);
+		}
+		
+		void add(double n3, double n4) {			
+			System.out.println(n3 + n4);
+		}
+
+		
+		
+		
+		
+		
+		
+		
+}
